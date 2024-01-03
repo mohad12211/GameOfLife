98d4a1910159a9140f2fc64b713e584d655f3dea
mohad12211 <mohammedkaabi64@gmail.com>
2024-01-04 00:42:33 +0300

    Infinite Grid

--- src/main.c
+++ src/main.c
@@ -1,89 +1,144 @@
+#include <assert.h>
+#include <float.h>
 #include <raylib.h>
+#include <raymath.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+#define STB_DS_IMPLEMENTATION
+#include "stb_ds.h"
 
 #define WIDTH 1600
 #define HEIGHT 900
-#define CELL_LEN 50
-#define ROW_COUNT (HEIGHT / CELL_LEN)
-#define COLUMN_COUNT (WIDTH / CELL_LEN)
+#define CELL_LEN 25
 #define CELL_COUNT (COLUMN_COUNT * ROW_COUNT)
-#define OFFSET (CELL_LEN / 2.0f)
+#define ZOOM_INCREMENT 0.050f
+#define MINIMUM_ZOOM 0.250f
+#define CELLS_INIT_CAPACITY 256
+
+typedef struct {
+  int x;
+  int y;
+} Cell;
+
+typedef struct {
+  Cell key;
+} CellEntry;
 
 int main(int argc, char *argv[]) {
-  bool grid[ROW_COUNT][COLUMN_COUNT] = {0};
 
   SetTraceLogLevel(LOG_WARNING);
-  InitWindow(WIDTH + OFFSET * 2, HEIGHT + OFFSET * 2, "GameOfLife");
+  InitWindow(WIDTH, HEIGHT, "GameOfLife");
   SetTargetFPS(60);
 
+  CellEntry *aliveCells = NULL;
+  CellEntry *nextAliveCells = NULL;
+  Camera2D camera = {0};
+  camera.zoom = 1.0f;
+  bool shouldDrawGrid = true;
+
   while (!WindowShouldClose()) {
-    BeginDrawing();
-    ClearBackground(WHITE);
+    // Zoom
+    float wheel = GetMouseWheelMove();
+    if (wheel != 0) {
+      Vector2 mouseWorldPosition = GetScreenToWorld2D(GetMousePosition(), camera);
+      camera.target = mouseWorldPosition;
+      camera.offset = GetMousePosition();
+      camera.zoom += (wheel * ZOOM_INCREMENT);
+      if (camera.zoom < MINIMUM_ZOOM)
+        camera.zoom = MINIMUM_ZOOM;
+    }
+
+    // Drag
+    if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
+      Vector2 delta = GetMouseDelta();
+      delta = Vector2Scale(delta, -1.0f / camera.zoom);
+      camera.target = Vector2Add(camera.target, delta);
+    }
 
     // Edit Cell (Toggle)
     if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-      int row = (GetMouseY() - OFFSET) / CELL_LEN;
-      int column = (GetMouseX() - OFFSET) / CELL_LEN;
-      if (row >= 0 && row < ROW_COUNT && column >= 0 && column < COLUMN_COUNT) {
-        grid[row][column] ^= true;
+      Vector2 mouseWorldPosition = GetScreenToWorld2D(GetMousePosition(), camera);
+      int y = floor(mouseWorldPosition.y / CELL_LEN);
+      int x = floor(mouseWorldPosition.x / CELL_LEN);
+      CellEntry entry = (CellEntry){(Cell){x, y}};
+      if (hmgeti(aliveCells, entry.key) >= 0) {
+        assert(hmdel(aliveCells, entry.key) == 1);
+      } else {
+        hmputs(aliveCells, entry);
       }
     }
 
     // Step to the next generation
     if (IsKeyPressed(KEY_SPACE)) {
-      bool newGrid[ROW_COUNT][COLUMN_COUNT] = {0};
-      for (int row = 0; row < ROW_COUNT; row++) {
-        for (int column = 0; column < COLUMN_COUNT; column++) {
+      Vector3 min = {FLT_MAX, FLT_MAX, 0};
+      Vector3 max = {FLT_MIN, FLT_MIN, 0};
+      for (int i = 0; i < hmlen(aliveCells); i++) {
+        min = Vector3Min(min, (Vector3){aliveCells[i].key.x, aliveCells[i].key.y, 0});
+        max = Vector3Max(max, (Vector3){aliveCells[i].key.x, aliveCells[i].key.y, 0});
+      }
+      for (int x = min.x - 1; x <= max.x + 1; x++) {
+        for (int y = min.y - 1; y <= max.y + 1; y++) {
           int aliveNeighbours = 0;
-          for (int dr = -1; dr <= 1; dr++) {
-            for (int dc = -1; dc <= 1; dc++) {
-              if (dr == 0 && dc == 0) {
+          for (int dx = -1; dx <= 1; dx++) {
+            for (int dy = -1; dy <= 1; dy++) {
+              if (dy == 0 && dx == 0) {
                 continue;
               }
-              int newRow = row + dr;
-              int newColumn = column + dc;
-              if (newRow < 0 || newRow >= ROW_COUNT || newColumn < 0 || newColumn >= COLUMN_COUNT) {
-                continue;
-              }
-              if (grid[newRow][newColumn]) {
+              Cell neighbourCell = {x + dx, y + dy};
+              if (hmgeti(aliveCells, neighbourCell) >= 0) {
                 aliveNeighbours++;
               }
             }
           }
-          bool alive = grid[row][column];
-          if (alive && (aliveNeighbours == 2 || aliveNeighbours == 3)) {
-            newGrid[row][column] = true;
-          } else if (!alive && aliveNeighbours == 3) {
-            newGrid[row][column] = true;
-          } else {
-            newGrid[row][column] = false;
+          CellEntry cellEntry = (CellEntry){(Cell){x, y}};
+          bool isAlive = hmgeti(aliveCells, cellEntry.key) >= 0;
+          if (isAlive && (aliveNeighbours == 2 || aliveNeighbours == 3)) {
+            hmputs(nextAliveCells, cellEntry);
+          } else if (!isAlive && aliveNeighbours == 3) {
+            hmputs(nextAliveCells, cellEntry);
           }
         }
       }
-      memcpy(grid, newGrid, CELL_COUNT);
+      hmfree(aliveCells);
+      aliveCells = nextAliveCells;
+      nextAliveCells = NULL;
     }
 
-    // Draw Cells
-    for (int row = 0; row < ROW_COUNT; row++) {
-      for (int column = 0; column < COLUMN_COUNT; column++) {
-        if (grid[row][column]) {
-          DrawRectangle(column * CELL_LEN + OFFSET, row * CELL_LEN + OFFSET, CELL_LEN, CELL_LEN, BLACK);
+    // Toggle grid drawing
+    if (IsKeyPressed(KEY_G)) {
+      shouldDrawGrid ^= true;
+    }
+
+    BeginDrawing();
+    ClearBackground(WHITE);
+    BeginMode2D(camera);
+
+    // Draw cells
+    for (int i = 0; i < hmlen(aliveCells); i++) {
+      DrawRectangle(aliveCells[i].key.x * CELL_LEN, aliveCells[i].key.y * CELL_LEN, CELL_LEN, CELL_LEN, BLACK);
+    }
+
+    // Draw Grid Lines
+    if (shouldDrawGrid) {
+      Vector2 topLeft = Vector2Scale(GetScreenToWorld2D(Vector2Zero(), camera), 1.0f / CELL_LEN);
+      Vector2 bottomRight = Vector2Scale(GetScreenToWorld2D((Vector2){WIDTH, HEIGHT}, camera), 1.0f / CELL_LEN);
+      for (int y = topLeft.y - 1; y <= bottomRight.y; y++) {
+        for (int x = topLeft.x - 1; x <= bottomRight.x; x++) {
+          Rectangle rec = {x * CELL_LEN, y * CELL_LEN, CELL_LEN, CELL_LEN};
+          DrawRectangleLinesEx(rec, 1, GRAY);
         }
       }
     }
 
-    // Draw Grid Lines
-    for (int row = 0; row < HEIGHT / CELL_LEN; row++) {
-      for (int column = 0; column < WIDTH / CELL_LEN; column++) {
-        DrawRectangleLines(column * CELL_LEN + OFFSET, row * CELL_LEN + OFFSET, CELL_LEN, CELL_LEN, GRAY);
-      }
-    }
-    DrawRectangleLines(OFFSET - 1, OFFSET - 1, WIDTH + 2, HEIGHT + 2, GRAY);
-
+    EndMode2D();
     EndDrawing();
   }
 
+  hmfree(aliveCells);
+  hmfree(nextAliveCells);
   CloseWindow();
   return 0;
 }
